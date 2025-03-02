08e77683081355dd61addefd3d5b8cf09e6456e3
nikhil <x11.niks@gmail.com>
2025-03-02 19:20:00 +0530

    first commit

---
+++ 01/hello.js
@@ -0,0 +1,4 @@
+const {add,sub} = require('./math');
+console.log(add(5,6)); // 42
+console.log(sub(5,6)); // -1
+// console.log(math(1,6)); // { add: [Function: add], subtract: [Function: subtract] }
